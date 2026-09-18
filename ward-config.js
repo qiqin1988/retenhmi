230be5ba230e2d1f93f9ef07090e8caf98fee328
qiqin1988 <qiqin288@gmail.com>
2026-09-18 16:45:32 +0800

    Tambah konfigurasi sumber data Wad 1

---
+++ ward-config.js
@@ -0,0 +1,10 @@
+// Konfigurasi sumber data Hospital Mesra Ibadah (HMI)
+// Ujian awal: 1 wad sahaja.
+// Spreadsheet ID = ID Google Sheet, BUKAN URL Apps Script.
+
+const WARD_CONFIG = [
+  {
+    name: 'Wad 1',
+    spreadsheetId: '1AGqZ8F3cXJJ0XcvPS2Frn-ksXlarzrtQ'
+  }
+];
