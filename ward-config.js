f097ad5c291b52beac546051d7d10ce0ba68bfe6
qiqin1988 <qiqin288@gmail.com>
2026-09-18 16:51:50 +0800

    Update HMI ward sources

--- ward-config.js
+++ ward-config.js
@@ -1,10 +1,14 @@
 // Konfigurasi sumber data Hospital Mesra Ibadah (HMI)
-// Ujian awal: 1 wad sahaja.
+// Setiap wad mempunyai Google Sheet sumber data masing-masing.
 // Spreadsheet ID = ID Google Sheet, BUKAN URL Apps Script.
 
 const WARD_CONFIG = [
   {
     name: 'Wad 1',
-    spreadsheetId: '1AGqZ8F3cXJJ0XcvPS2Frn-ksXlarzrtQ'
+    spreadsheetId: '1vebPCSJt_VO6qnXMkf-GnPyJlJ0rVnnj'
+  },
+  {
+    name: 'Wad 2',
+    spreadsheetId: '1JbH64ennamHmiJRiuZzq39dyZeOPlleC'
   }
 ];
